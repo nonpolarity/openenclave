aca38259ee93dc2817aee8b114ef9d5d74b59507
Alvin Chen <alvin@chen.asia>
2020-04-14 23:26:32 -0700

    Fix rmdir.

--- tests/syscall/fs/host/host.c
+++ tests/syscall/fs/host/host.c
@@ -3,6 +3,11 @@
 
 #if defined(_WIN32)
 #include <windows.h>
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
+#pragma comment(lib, "User32.lib")
 #else
 #include <dirent.h>
 #endif
@@ -98,6 +103,70 @@ int rmdir(const wchar_t* sPath)
 
     return !RemoveDirectoryW(sPath); // remove the empty directory
 }
+int rmdir2(const TCHAR* path)
+{
+   WIN32_FIND_DATA ffd;
+   LARGE_INTEGER filesize;
+   TCHAR szDir[MAX_PATH];
+   size_t length_of_arg;
+   HANDLE hFind = INVALID_HANDLE_VALUE;
+   DWORD dwError=0;
+
+   // If the directory is not specified as a command-line argument,
+   // print usage.
+
+   StringCchLength(path, MAX_PATH, &length_of_arg);
+
+   if (length_of_arg > (MAX_PATH - 3))
+   {
+//      _tprintf(TEXT("\nDirectory path is too long.\n"));
+      return (-1);
+   }
+
+//   _tprintf(TEXT("\nTarget directory is %s\n\n"), argv[1]);
+
+   // Prepare string for use with FindFile functions.  First, copy the
+   // string to a buffer, then append '\*' to the directory name.
+
+   StringCchCopy(szDir, MAX_PATH, path);
+   StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+
+   // Find the first file in the directory.
+
+   hFind = FindFirstFile(szDir, &ffd);
+
+   if (INVALID_HANDLE_VALUE == hFind)
+   {
+//      DisplayErrorBox(TEXT("FindFirstFile"));
+      return dwError;
+   }
+
+   // List all the files in the directory with some info about them.
+
+   do
+   {
+      if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+      {
+//         _tprintf(TEXT("  %s   <DIR>\n"), ffd.cFileName);
+      }
+      else
+      {
+         filesize.LowPart = ffd.nFileSizeLow;
+         filesize.HighPart = ffd.nFileSizeHigh;
+//         _tprintf(TEXT("  %s   %ld bytes\n"), ffd.cFileName, filesize.QuadPart);
+      }
+   }
+   while (FindNextFile(hFind, &ffd) != 0);
+
+   dwError = GetLastError();
+   if (dwError != ERROR_NO_MORE_FILES)
+   {
+//      DisplayErrorBox(TEXT("FindFirstFile"));
+   }
+
+   FindClose(hFind);
+   return dwError;
+}
 
 int wmain(int argc, wchar_t* argv[])
 {
@@ -141,7 +210,7 @@ int wmain(int argc, wchar_t* argv[])
     // Windows does not support umask.
     // Please set up the right permission to the parent directory.
 
-    if(rmdir(argv[1]))
+    if(rmdir(argv[3]))
     {
         fprintf(stdout, "Remove dir failed!\n");
     }
