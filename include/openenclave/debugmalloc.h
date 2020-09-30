d2bae6f14627c8ebf7e04bbbc593fc4ea3ed1c8d
Alvin Chen <alvin@chen.asia>
2020-09-29 22:25:51 -0700

    Add support to cplusplus for debugmalloc.h
    Signed-off-by: Alvin Chen <alvin@chen.asia>

--- include/openenclave/debugmalloc.h
+++ include/openenclave/debugmalloc.h
@@ -4,10 +4,20 @@
 #ifndef _OE_DEBUG_MALLOC_H
 #define _OE_DEBUG_MALLOC_H
 
+#ifdef __cplusplus
+extern "C"
+{
+    void oe_debug_malloc_start_tracking(void);
+
+    void oe_debug_malloc_stop_tracking(void);
+
+    void oe_debug_malloc_print_objects(void);
+}
+#else
 void oe_debug_malloc_start_tracking(void);
 
 void oe_debug_malloc_stop_tracking(void);
 
 void oe_debug_malloc_print_objects(void);
-
+#endif
 #endif /* _OE_DEBUG_MALLOC_H */
